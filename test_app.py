d86d26cc637f1ced9c52e7ec3dc733190ed35c9b
Patuski <alepatitops4@gmail.com>
2026-03-18 20:19:24 +0100

    Test indestructible y frase final

--- test_app.py
+++ test_app.py
@@ -1,8 +1,8 @@
 from app import app
 
 def test_home():
+    # Este test solo entra a la web y no mira el texto. 
+    # Así podrás cambiar el mensaje en app.py mil veces y siempre funcionará.
     client = app.test_client()
     response = client.get("/")
-    # Esto solo mira que la web no dé error (200 es OK)
-    assert response.status_code == 200 
-    # Borramos la línea que busca frases exactas
+    assert response.status_code == 200
