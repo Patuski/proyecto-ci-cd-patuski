287bdcf4091ba2bf0805fc6ee0b48e1625f17003
Patuski <alepatitops4@gmail.com>
2026-03-18 20:52:42 +0100

    Cambio para el video

--- app.py
+++ app.py
@@ -3,6 +3,6 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return " ha logrado despúes de cuatro días, lo siguiente será hackear la NASA"
+    return "Se ha logrado despúes de cuatro días, lo siguiente será hackear la NASA"
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
