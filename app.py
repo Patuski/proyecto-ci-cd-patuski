045f1cc3d1eac8537145b439ebc946864a4dc45a
Patuski <alepatitops4@gmail.com>
2026-03-18 20:05:41 +0100

    Corrigiendo error de sintaxis en el mensaje

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
 from flask import Flask
-
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return "Despues de tres/cuatro días he conseguido llegar a este punto, lo siguiente será hackear la NASA"
+@app.route('/')
+def hello_world():
+    return "¡Hola Patuski! Mi primer Pipeline CI/CD funciona al 100%."
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
