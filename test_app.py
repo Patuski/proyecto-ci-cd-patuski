ea0ecc8139a9f7de883bc1de353f59d36c836be3
Patuski <alepatitops4@gmail.com>
2026-03-18 20:14:00 +0100

    Sincronizando el test con el nuevo mensaje de Patuski

--- test_app.py
+++ test_app.py
@@ -4,4 +4,6 @@ def test_home():
     client = app.test_client()
     response = client.get("/")
     assert response.status_code == 200
-    assert b"Patuski" in response.data
+    # Cambiamos esto para que busque cualquier cosa que empiece por "se ha logrado"
+    assert b"se ha logrado" in response.data
+
