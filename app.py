9214df993af9a6594cc3da7c1fb9c238f251d304
Patuski <alepatitops4@gmail.com>
2026-03-18 21:11:58 +0100

    Frase del hackeo final

--- app.py
+++ app.py
@@ -3,6 +3,6 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return "Hola, que tal?"
+    return "Se ha logrado después de cuatro días, Estoy cansado profe"
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
