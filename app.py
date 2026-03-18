4b5f2e8ab3cdea8ef26b9c94e977038e6156fe44
Patuski <alepatitops4@gmail.com>
2026-03-18 21:06:14 +0100

    Actualizando frase para el video

--- app.py
+++ app.py
@@ -3,6 +3,6 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return "Se ha logrado despúes de cuatro días, lo siguiente será hackear la NASA"
+    return "Hola, que tal?"
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
