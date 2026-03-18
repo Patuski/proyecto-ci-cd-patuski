d2a43198dcd3f502debb8c7b3771367c0c92a6cb
Patuski <alepatitops4@gmail.com>
2026-03-18 20:02:32 +0100

    Paso 9: Prueba final de automatización

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Servidor desplegado correctamente en Kubernetes!"
+    return "Despues de tres/cuatro días he conseguido llegar a este punto, lo siguiente será hackear la NASA"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
